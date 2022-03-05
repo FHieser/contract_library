fc66af3182226dbb1b048beaac155c4359848cff
FHieser <47272854+FHieser@users.noreply.github.com>
2022-03-05 16:43:47 +0100

    Adding Airdrop Test

---
+++ test/airdrop_test.js
@@ -0,0 +1,39 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Airdrop_flat Unit Test", function () {
+
+    //Get List of created accounts and put them in a array 
+    before(async function () {
+        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        this.recipients = [owner.address, addr1.address, addr2.address, addr3.address];
+        this.Contract = await ethers.getContractFactory("Airdrop");
+    })
+    //deploy contract
+    beforeEach(async function () {
+        this.contract = await this.Contract.deploy("name", "symbol", "URI");
+
+        await this.contract.deployed();
+    })
+
+
+    it("Send Batch: One NFT in each of the recipients wallets", async function () {
+        // Send one NFT to each Recipient address
+        await this.contract.sendBatch(this.recipients);
+
+        for (address of this.recipients) {
+
+            //Check if each address owns exactly 1 token
+            expect(await this.contract.balanceOf(address)).to.equal(1);
+        };
+    });
+    it("Send Batch: The minted Token amount should equal the amount of recipients", async function () {
+        // Send one NFT to each Recipient address
+        await this.contract.sendBatch(this.recipients);
+
+        //Check if amount of NFTs equals the amount of recipients
+        expect(await this.contract.totalSupply()).to.equal(this.recipients.length);
+
+    });
+
+});
