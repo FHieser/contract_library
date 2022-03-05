b7a75a7d22c535d16a356fac40616aef30c2a5d8
FHieser <47272854+FHieser@users.noreply.github.com>
2022-03-05 17:01:44 +0100

    Create revealable_optimized_test.js

---
+++ test/revealable_optimized_test.js
@@ -0,0 +1,48 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Revealable_optimized_flat Unit Test", function () {
+
+    
+
+    //Get List of created accounts and put them in a array 
+    before(async function () {
+        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        this.recipients = [owner.address, addr1.address, addr2.address, addr3.address];
+        this.Contract = await ethers.getContractFactory("RevealableOptimizedContract");
+        
+        this.directURI = "directURI";
+        this.notRevealedURI = "notRevealedURI";
+    })
+    //deploy contract
+    beforeEach(async function () {
+        this.contract = await this.Contract.deploy("name", "symbol", this.directURI, this.notRevealedURI);
+        await this.contract.deployed();
+    })
+
+
+    it("tokenURI: Not revealed yet -> Returns notRevealedURI", async function () {
+        //mint a two token
+        await this.contract.mint(2);
+
+        //We didn´t change the revealed status so it should return the notRevealedURI
+        expect(await this.contract.tokenURI(0)).to.equal(this.notRevealedURI);
+        expect(await this.contract.tokenURI(1)).to.equal(this.notRevealedURI);
+    });
+
+    it("tokenURI: Revealed -> Returns directURI + Filename of metadata", async function () {
+        //mint a two token
+        await this.contract.mint(2);
+
+        //Revealing the metadata
+        await this.contract.reveal();
+
+        //Creating the comparable tokenStrings, that point towards the metadata of the tokens
+        let tokenString1=this.directURI+"0.json"
+        let tokenString2=this.directURI+"1.json"
+
+        expect(await this.contract.tokenURI(0)).to.equal(tokenString1);
+        expect(await this.contract.tokenURI(1)).to.equal(tokenString2);
+    });
+
+});
