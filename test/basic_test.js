0d580fd018c22d3de0d6deb59899b7096d5b3a83
FHieser <47272854+FHieser@users.noreply.github.com>
2022-03-06 12:41:24 +0100

    Test Minting Function of basic

--- test/basic_test.js
+++ test/basic_test.js
@@ -5,8 +5,8 @@ describe("Basic_flat Unit Test", function () {
 
     //Get List of created accounts and put them in a array 
     before(async function () {
-        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
-        this.recipients = [owner.address, addr1.address, addr2.address, addr3.address];
+        [this.owner, this.addr1, this.addr2, this.addr3] = await ethers.getSigners();
+        this.recipients = [this.owner.address, this.addr1.address, this.addr2.address, this.addr3.address];
         this.Contract = await ethers.getContractFactory("BasicNFT");
 
         this.name = "name";
@@ -29,6 +29,41 @@ describe("Basic_flat Unit Test", function () {
         //Check Symbol
         expect(await this.contract.symbol()).to.equal(this.symbol);
     });
+
+    it("Mint: while paused", async function () {
+        //pause
+        await this.contract.pause(true);
+
+        await expect(this.contract.mint(1)).to.be.revertedWith("Contract is paused");
+    });
+
+    it("Mint: Mint Amount less than 1", async function () {
+        await expect(this.contract.mint(0)).to.be.revertedWith("Mint Amount needs to be bigger than 0");
+    });
+
+    //Max Mint Amount is 20
+    it("Mint: Mint Amount more than MaxMintAmount", async function () {
+        await expect(this.contract.mint(21)).to.be.revertedWith("Mint Amount exceeds the Maximum Allowed Mint Amount");
+    });
+
+    it("Mint: Mint Amount more than AvailableMintAmount", async function () {
+        //Set new maxMintAmount to 10000
+        await this.contract.setmaxMintAmount(10000);
+        //Mint to full capacity
+        await this.contract.mint(10000);
+        //Mint over limit
+        await expect(this.contract.mint(1)).to.be.revertedWith("Mint Amount exceeds the Available Mint Amount");
+    });
+
+    it("Mint: Owner mints for free", async function () {
+        await this.contract.connect(this.owner).mint(1,{value:0});
+    });
+
+    it("Mint: Owner mints for free", async function () {
+        await expect(this.contract.connect(this.addr1).mint(1,{value:0})).to.be.revertedWith("Value for minting-transaction is to low");
+    });
+
+
 
     it("tokenURI: Returns correctURI", async function () {
         //mint a two token
