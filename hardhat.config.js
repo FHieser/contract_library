bf361e19bb6705a7edfb0a42658cc63cae9bed6f
FHieser <47272854+FHieser@users.noreply.github.com>
2022-03-12 23:50:47 +0100

    Adding hardhat gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 
+
+
+require("hardhat-gas-reporter");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  gasReporter: {
+    currency: 'EUR',
+    token:'ETH',
+    gasPriceApi:'	https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
+  }
 };
+
